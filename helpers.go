fc6b15457222e0414f9b19cf4194c56ec7201e4d
Tyler Young <tyler.young@impromptu.ninja>
2024-07-18 02:05:39 -0400

    only handle messages that start with `:buy`
    add showMsg helper from chirp24/autogarland
    
    add feedback during bulk purchase operation
    
    add fail msg with instruction due to no recorded purchase

---
+++ helpers.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"xabbo.b7c.io/goearth/shockwave/in"
+	"xabbo.b7c.io/goearth/shockwave/profile"
+	"xabbo.b7c.io/goearth/shockwave/room"
+)
+
+func showMsg(msg string) {
+	self := roomMgr.EntityByName(profileMgr.Name)
+	if self == nil {
+		// fmt.Println("self not found.")
+		return
+	}
+	ext.Send(in.CHAT, self.Index, msg)
+}
+
+
+var roomMgr = room.NewManager(ext)
+var profileMgr = profile.NewManager(ext)
